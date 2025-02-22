2838f8cbec723e9ba28ee45a444bf653432f4ac3
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2025-02-22 16:37:50 +0300

    Got user and course models working Fixed Typos

--- server/models.py
+++ server/models.py
@@ -6,8 +6,8 @@ from config import db, metadata
 enrollments = db.Table(
     "enrollments",
     metadata,
-    db.Column("student_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
-    db.Column("course_id", db.Integer, db.ForeignKey("course.id"), primary_key=True)
+    db.Column("student_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column("course_id", db.Integer, db.ForeignKey("courses.id"), primary_key=True)
 )
 
 #User Table
@@ -22,7 +22,10 @@ class User(db.Model, SerializerMixin):
     created_at = db.Column(db.String, nullable = False)
 
     #Relationships
-    courses = db.relationship("Course", secondary=enrollments, back_populates="students")
+    courses = db.relationship("Course", secondary = enrollments, back_populates="students")
+
+    # Serialization rules
+    serialize_rules = ('-courses.students',)
 
     def __repr__(self):
         return f'<User {self.id}, Name: {self.name}, Role: {self.role}>'
@@ -39,14 +42,14 @@ class Course(db.Model, SerializerMixin):
     created_at = db.Column(db.String, nullable=False)
 
     # Relationships
-    teacher = db.relationship('User', foreign_keys=[teacher_id], back_populates='courses_taught')
-    lessons = db.relationship('Lesson', back_populates='course', cascade='all, delete-orphan')
-    enrollments = db.relationship('Enrollment', back_populates='course', cascade='all, delete-orphan')
-    progress = db.relationship('Progress', back_populates='course', cascade='all, delete-orphan')
-    certificates = db.relationship('Certificate', back_populates='course', cascade='all, delete-orphan')
+    students = db.relationship('User', secondary = enrollments, back_populates='courses')
+    # lessons = db.relationship('Lesson', back_populates='course', cascade='all, delete-orphan')
+    # progress = db.relationship('Progress', back_populates='course', cascade='all, delete-orphan')
+    # certificates = db.relationship('Certificate', back_populates='course', cascade='all, delete-orphan')
 
     # Serialization rules
-    serialize_rules = ('-teacher.courses_taught', '-lessons.course', '-enrollments.course', '-progress.course', '-certificates.course')
+    serialize_rules = ('-students.courses',)
+    # serialize_rules = ('-teacher.courses_taught', '-lessons.course', '-enrollments.course', '-progress.course', '-certificates.course')
 
     def __repr__(self):
         return f"<Course {self.title}>"
