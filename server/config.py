ee6058fe70c8b12117f60b1dba7c309541c8f06f
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2025-02-23 20:33:48 +0300

    Added bcrypt to config

--- server/config.py
+++ server/config.py
@@ -5,9 +5,11 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from sqlalchemy import MetaData
 
 app = Flask(__name__)
+flask_bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
